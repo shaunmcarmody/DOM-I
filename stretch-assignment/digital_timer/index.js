224b4eb93b74f7ca21bf8ffc60122bb76f5d2d79
c87 <shaun.carmody@live.co.uk>
2019-01-29 12:05:44 +0000

    stretch complete

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,4 +1,5 @@
 const time = {
+    active: false,
     secondTens: 0,
     secondOnes: 0,
     msHundreds: 0,
@@ -13,36 +14,66 @@ const updateTime = () => {
     return;
 }
 
-setInterval(() => {
-    if (time.secondTens >= 1) {
-        document.querySelector('.digits').style = "color: red";
-        return;
-    }
-    time.msTens += 1;
+const go = () => {
+    setInterval(() => {
+        if (!time.active) return;
 
-    if (time.msTens >= 10) {
-        time.msHundreds += 1;
-        time.msTens = 0;
+        if (time.secondTens >= 1) {
+            document.querySelector('.digits').style = "color: red";
+            return;
+        }
+
+        time.msTens += 1;
         updateTime();
-    }
 
-    if (time.msHundreds >= 10) {
-        time.secondOnes += 1;
-        time.msHundreds = 0;
-        time.msTens = 0;
-        updateTime();
-    }
+        if (time.msTens >= 10) {
+            time.msHundreds += 1;
+            time.msTens = 0;
+            updateTime();
+        }
 
-    if (time.secondOnes >= 10) {
-        time.secondTens = 1;
-        time.secondOnes = 0;
-        time.msHundreds = 0;
-        time.msTens = 0;
-        updateTime();
-        return;
-    }
-}, 10);
+        if (time.msHundreds >= 10) {
+            time.secondOnes += 1;
+            time.msHundreds = 0;
+            time.msTens = 0;
+            updateTime();
+        }
 
-document.getElementById('secondTens').addEventListener('change', (e) => {
-    console.log('10 seconds');
+        if (time.secondOnes >= 10) {
+            time.secondTens = 1;
+            time.secondOnes = 0;
+            time.msHundreds = 0;
+            time.msTens = 0;
+            updateTime();
+            return;
+        }
+    }, 10);    
+}
+
+const buttons = document.querySelector('.buttons');
+
+const start = document.createElement('button');
+start.textContent = 'Start';
+const reset = start.cloneNode();
+reset.textContent = 'Reset'
+
+buttons.appendChild(start);
+buttons.appendChild(reset);
+
+start.addEventListener('click', (e) => {
+    time.active = true;
+    e.target.setAttribute('disabled', true);
+    go();
 });
+
+reset.addEventListener('click', (e) => {
+    document.querySelector('.digits').style = "color: black";
+    time.active = false;
+    time.secondTens = 0;
+    time.secondOnes = 0;
+    time.msHundreds = 0;
+    time.msTens = 0;
+    updateTime();
+    start.removeAttribute('disabled');
+});
+
